340e4ef688b1ba568f11394ac9218b913feca60c
postemp <apostemp@mail.ru>
2023-10-01 20:38:48 +0300

    hw25

--- chat-server/src/main/java/Main.java
+++ chat-server/src/main/java/Main.java
@@ -2,17 +2,9 @@ import java.sql.*;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-/*
- user(id, login, password)
- roles(id, roles_name)
- user_to_roles(user_id, role_id))
- */
 
 public class Main {
-
     public static void main(String[] args) {
-
-
         Server server = new Server(8080, new InMemoryAuthenticationProvider());
         server.start();
     }
